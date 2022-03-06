e51a9800f7c6d0d917f140cd2f17d626a63d07c9
ahhh <whittyq800@163.com>
2022-03-06 13:37:39 +0800

    feat: 新增批量修改

--- src/main/main.js
+++ src/main/main.js
@@ -25,7 +25,10 @@ module.exports = {
         click(event, data) {
             // Editor.log("点击提交..", data);
             if (data) {
-                startMain(data, ConfigManager.get().id);
+                let dataArr = data.split(",");
+                Editor.log("获得的id:", dataArr);
+                startMain(dataArr, ConfigManager.get().id);
+                // startMain(data, ConfigManager.get().id);
             }
         },
         initpanel() {
@@ -56,44 +59,54 @@ module.exports = {
  * @param {*} courseid 课程id
  * @param {*} id 登录id
  */
-async function startMain(courseid, id) {
+async function startMain(courseidArr, id) {
     var request = require('request');
     //post方式请求
     var url = 'https://pipixia.mathufo.com/practice/problem_set/get_all_by_id';
-    request({
-        url: url,
-        method: 'post',
-        json: true,
-        headers: {
-            'content-type': 'application/json',
-            "cookie": "sessionid=" + id,
-        },
-        body: {
-            id: courseid,
-            lang: "zh-cn"
-        },
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            let version = body.data.current_version;
-            Editor.log("open===>", body.data.current_version); // 请求成功的处理逻辑
-            let url = pathData.join(ConfigManager.get().pipixiaPath + "/" + version + ".txt");
-            const ws = Fs.createWriteStream(url, { autoClose: true });
-            request("https://zyj-bj.oss-cn-beijing.aliyuncs.com/mathup/problems/" + version).pipe(ws);
-            ws.on("finish", function (data) {
-                Editor.log("<===下载完成===>", data);
-                ws.close();
-                Editor.assetdb.refresh("db://assets", function (err, results) {
-                    Editor.log('updata asset!');
-                    getLevelConfig(courseid, version);
+    var versionArr = new Map();
+
+    for (let i = 0; i < courseidArr.length; i++) {
+        request({
+            url: url,
+            method: 'post',
+            json: true,
+            headers: {
+                'content-type': 'application/json',
+                "cookie": "sessionid=" + id,
+            },
+            body: {
+                id: courseidArr[i],
+                lang: "zh-cn"
+            },
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                let version = body.data.current_version;
+                Editor.log("open===>", body.data.current_version); // 请求成功的处理逻辑
+                let url = pathData.join(ConfigManager.get().pipixiaPath + "/" + version + ".txt");
+                const ws = Fs.createWriteStream(url, { autoClose: true });
+                request("https://zyj-bj.oss-cn-beijing.aliyuncs.com/mathup/problems/" + version).pipe(ws);
+                ws.on("finish", function (data) {
+                    Editor.log("<===下载完成===>", data);
+                    ws.close();
+                    versionArr.set(parseInt(courseidArr[i]), version);
+                    if (versionArr.size === courseidArr.length) {
+
+                        let pipixiaData = ConfigManager.get().pipixiaPath;
+                        let pipixiaUrl = pipixiaData.split("assets");
+                        Editor.assetdb.refresh("db://assets/" + pathData.join(pipixiaUrl[pipixiaUrl.length - 1]), function (err, results) {
+                            getLevelConfig(versionArr);
+                        });
+                    }
                 });
-            });
-        }
-    });
+            }
+        });
+    }
 }
+
 /**
- * 修改config里的内容
- */
-async function getLevelConfig(courseid, versionid) {
+  * 修改config里的内容
+  */
+async function getLevelConfig(versionSet) {
     var configPath = ConfigManager.get().config + ".json";
     let file = await FileUtil.readFile(configPath);
     let data = null;
@@ -106,34 +119,99 @@ async function getLevelConfig(courseid, versionid) {
     if (!data) {
         return null;
     }
-    await setLevelConfig(configPath, courseid, data, versionid);
+    await setLevelConfig(configPath, data, versionSet);
     return data;
 }
 /**
- * 修改confi配置
- * @param {*} configPath config路径
- * @param {*} courseid 课件id
- * @param {*} data 皮皮虾配置文件
- * @param {*} versionid 皮皮虾名
- */
-async function setLevelConfig(configPath, courseid, data, versionid) {
+  * 修改config配置
+  * @param {*} configPath config路径
+  * @param {*} courseid 课件id
+  * @param {*} data 皮皮虾配置文件
+  * @param {*} versionid 皮皮虾名
+  */
+async function setLevelConfig(configPath, data, versionSet) {
     let new_str = JSON.stringify(data);
     let new_json = JSON.parse(new_str);
-    var index = 0;
-    var jsonData
+    var indexArr = [];
+    var jsonDataArr = []
+    // Editor.log("准备修改config", new_json["levels"]);
     for (let k in new_json["levels"]) {
         let json = new_json["levels"][k];
-        if (new_json["levels"][k]["ppx_id"] == courseid) {
-            json["data"] = versionid + "";
-            index = parseInt(k);
-            jsonData = json;
-        }
+        let index = 0;
+        versionSet.forEach(function (value, key, map) {
+            if (new_json["levels"][k]["ppx_id"] == key) {
+                json["data"] = value + "";
+                indexArr.push(parseInt(index));
+                jsonDataArr.push(json)
+            }
+            index++;
+        });
     }
-    new_json["levels"][index] = jsonData;
+    for (let i = 0; i < indexArr.length; i++) {
+        new_json["levels"][indexArr[i]] = jsonDataArr[i];
+    }
     Fs.writeFile(configPath, JSON.stringify(new_json), 'utf-8', function (arr) {
+
+        let configData = ConfigManager.get().config;
+        let pipixiaData = ConfigManager.get().pipixiaPath;
+        let configUrl = configData.split("assets");
+        let pipixiaUrl = pipixiaData.split("assets");
         Editor.log('------modify successfully!------');
-        Editor.assetdb.refresh("db://assets", function (err, results) {
+        Editor.assetdb.refresh("db://assets/" + pathData.join(pipixiaUrl[pipixiaUrl.length - 1]), function (err, results) {
+            Editor.log('updata asset!');
+        });
+        Editor.assetdb.refresh("db://assets/" + pathData.join(configUrl[configUrl.length - 1]), function (err, results) {
             Editor.log('updata asset!');
         });
     });
 }
+
+
+
+// /**
+//  * 修改config里的内容
+//  */
+// async function getLevelConfig(courseid, versionid) {
+//     var configPath = ConfigManager.get().config + ".json";
+//     let file = await FileUtil.readFile(configPath);
+//     let data = null;
+//     try {
+//         data = JSON.parse(file);
+//     } catch (error) {
+//         Editor.log('File parsing failure!', path);
+//         Editor.log('Error:', error);
+//     }
+//     if (!data) {
+//         return null;
+//     }
+//     await setLevelConfig(configPath, courseid, data, versionid);
+//     return data;
+// }
+// /**
+//  * 修改confi配置
+//  * @param {*} configPath config路径
+//  * @param {*} courseid 课件id
+//  * @param {*} data 皮皮虾配置文件
+//  * @param {*} versionid 皮皮虾名
+//  */
+// async function setLevelConfig(configPath, courseid, data, versionid) {
+//     let new_str = JSON.stringify(data);
+//     let new_json = JSON.parse(new_str);
+//     var index = 0;
+//     var jsonData
+//     for (let k in new_json["levels"]) {
+//         let json = new_json["levels"][k];
+//         if (new_json["levels"][k]["ppx_id"] == courseid) {
+//             json["data"] = versionid + "";
+//             index = parseInt(k);
+//             jsonData = json;
+//         }
+//     }
+//     new_json["levels"][index] = jsonData;
+//     Fs.writeFile(configPath, JSON.stringify(new_json), 'utf-8', function (arr) {
+//         Editor.log('------modify successfully!------');
+//         Editor.assetdb.refresh("db://assets", function (err, results) {
+//             Editor.log('updata asset!');
+//         });
+//     });
+// }
